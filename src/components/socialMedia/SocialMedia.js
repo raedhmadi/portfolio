0944edb18fc8beae6f6dbb3479a9ae80166a8567
Ayla Hmadi <aylahmadi2016@gmail.com>
2025-01-01 18:23:27 +0200

    initialize

---
+++ src/components/socialMedia/SocialMedia.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./SocialMedia.scss";
+import {socialMediaLinks} from "../../portfolio";
+
+export default function socialMedia() {
+  if (!socialMediaLinks.display) {
+    return null;
+  }
+  return (
+    <div className="social-media-div">
+      {socialMediaLinks.linkedin ? (
+        <a
+          href={socialMediaLinks.linkedin}
+          className="icon-button linkedin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-linkedin-in"></i>
+          <span></span>
+        </a>
+      ) : null}
+
+      {socialMediaLinks.gmail ? (
+        <a
+          href={`mailto:${socialMediaLinks.gmail}`}
+          className="icon-button google"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fas fa-envelope"></i>
+          <span></span>
+        </a>
+      ) : null}
+
+
+      {socialMediaLinks.stackoverflow ? (
+        <a
+          href={socialMediaLinks.stackoverflow}
+          className="icon-button stack-overflow"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-stack-overflow"></i>
+          <span></span>
+        </a>
+      ) : null}
+    </div>
+  );
+}
